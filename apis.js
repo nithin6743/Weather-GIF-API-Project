b74a7970df6a4977482dbca5afe8842df0bce5d1
nithin6743 <nithinkumarsottagari03@gmail.com>
2025-02-01 22:31:45 +0530

    uploading html and css files

---
+++ apis.js
@@ -0,0 +1,23 @@
+let weather_api = "https://api.weatherapi.com/v1/current.json?key=9025db8dbabb430d952165219252901&q=17.465224782058666,78.54138010704175";
+
+let pet_api = "https://api.giphy.com/v1/gifs/{GIF_ID}?api_key=xbzOMuOZIy2p0uPqHHyAjhs6QrQNQKht";
+
+let Clear = "wHUWuJWg6iRRQWTBd1";
+let Sunny = "khzLJR9sTppTUpBEia";
+let Partly_Cloudy = "Oh1rKdcE3IFOpfeimr";
+let Cloudy = "Oh1rKdcE3IFOpfeimr";
+let Overcast = "Oh1rKdcE3IFOpfeimr";
+let Mist = "dUR62cwTf5aMGMOmwy";
+let Fog = "dUR62cwTf5aMGMOmwy";
+let Light_Rain = "AQ60Mqpz7sJLK4DiOW";
+let Heavy_Rain = "yGhTOpxFbAUhuBJG30";
+let Thunderstorm = "eyoDFbM8UwpIsjG8iI"
+let Light_Snow = "dwdBXHwYtuQV21eeF7";
+let Heavy_Snow = "lOgHL3b6ogY6IEkq6q";
+let Sleet = "dUR62cwTf5aMGMOmwy";
+let Hail = "8hhfYrbc0XugC1XIeO";
+let Windy = "lOgHL3b6ogY6IEkq6q";
+let Hot = "xhC6odm0c5kFZQ5MGh";
+let Cold = "dUR62cwTf5aMGMOmwy";
+let Freezing_Fog = "dwdBXHwYtuQV21eeF7";
+let Drizzle= "AQ60Mqpz7sJLK4DiOW";
---
+++ script.js
@@ -0,0 +1,42 @@
+// Initialize the map India coordinates
+let map = L.map('map').setView([22.5937, 78.9629], 5);
+
+// Add OpenStreetMap layer
+let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+osm.addTo(map);
+
+// Variable to store the marker
+let currentMarker;
+// Handle map clicks
+let latitude, longitude;
+
+
+map.on('click', function (e) {
+  let { lat, lng } = e.latlng;
+
+  latitude = lat;
+  longitude = lng;
+  console.log(`Selected Coordinates: Latitude: ${latitude}, Longitude: ${longitude}`);
+  // Remove the previous marker if it exists
+  if (currentMarker) {
+    map.removeLayer(currentMarker);
+  }
+
+  // Add a new marker at the clicked location
+  currentMarker = L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(`Coordinates:<br> Latitude: ${lat.toFixed(5)}, Longitude: ${lng.toFixed(5)}`)
+    .openPopup();
+});
+
+// tile layer(Stadia_OSM)
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	ext: 'png'
+});
+Stadia_AlidadeSmoothDark.addTo(map);
+
+
+
+L.Control.geocoder().addTo(map);
